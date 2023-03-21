208893028ef97d6c4f48fb8ce9f93054d70e779a
Joshua Melvin <joshuamlvn@gmail.com>
2023-03-21 10:45:19 +0700

    First init

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'siegsesta',
+    appId: '1:408387139975:web:510963d241b2db5cfb7b22',
+    storageBucket: 'siegsesta.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCgY0IpSwOO8pVRxFlmvloZ4gVusLnP5fk',
+    authDomain: 'siegsesta.firebaseapp.com',
+    messagingSenderId: '408387139975',
+    measurementId: 'G-SQ9FCXFYXD',
+  },
+};
